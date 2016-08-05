ef359574fffcb34c4f7f2da1bd694e1d7b3afc8f
Conlain Kelly <conlain.k@gmail.com>
2016-08-04 19:09:12 -0500

    Further cleaned out useless makefiles

---
+++ template_project/CMakeFiles/myexe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myexe.dir/VehicleServer.cpp.o"
+  "CMakeFiles/myexe.dir/home/conlain/Desktop/Programs/SBEL/ChronoNetworking/Vehicle_Protobuf_Messages/ChronoMessages.pb.cc.o"
+  "myexe.pdb"
+  "myexe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
